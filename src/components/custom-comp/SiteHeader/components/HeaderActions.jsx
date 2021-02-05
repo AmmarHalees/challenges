4abd263995829ea8a5ee881886d3c9c6cc208769
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2021-02-05 17:05:26 +0200

    Almost done with heade

--- src/components/custom-comp/SiteHeader/components/HeaderActions.jsx
+++ src/components/custom-comp/SiteHeader/components/HeaderActions.jsx
@@ -8,7 +8,7 @@ const HeaderActions = () => {
 
         <SearchIcon />
 
-        <Button type="primary" size="large" >hi</Button>
+        <Button type="primary" size="large" >Menu</Button>
 
 
     </div>);
--- src/components/library-comp/Button/Button.jsx
+++ src/components/library-comp/Button/Button.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Button.module.css';
 
-const Button = ({ title, children, onclick, type , size, disabled = false , loading = false }) => {
+const Button = ({ title, children, onclick, type , disabled = false , loading = false }) => {
     return ( <button disabled={disabled || loading} className={`${styles.Button} ${styles[type]}`} onClick={onclick} title={title} >
     
     {children}
