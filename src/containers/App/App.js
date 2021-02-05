05874a241ed8da49fce65e53419742e1810a879d
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2021-02-05 16:14:50 +0200

    working on structure

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,9 +1,12 @@
-import './App.css';
+import Button from '../../components/library-comp/Button/Button';
+
 
 function App() {
   return (
     <div className="App">
-    
+      
+    <Button type="primary" size="large">hi</Button>
+
     </div>
   );
 }
