a2939ddb5fcd8643312f8a0aa3a77afbc7212539
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2021-02-06 21:20:39 +0200

    working on service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './containers/App/App.jsx';
 import  "./assets/css/globals.css";
 
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -12,4 +11,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
