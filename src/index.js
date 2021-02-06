6a5df5f009dae91e18e73d784a75a87a0eedf4e5
Ammar Halees <32368546+AmmarHalees@users.noreply.github.com>
2021-02-06 22:35:46 +0200

    service worker fix

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './containers/App/App.jsx';
 import  "./assets/css/globals.css";
-
+// import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,3 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// serviceWorker.register();
